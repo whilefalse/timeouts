0ae4c57660cbbc10194d45f5a0473fed9e67bc4f
Steven Anderson <sanderson@sopost.com>
2021-09-16 11:39:51 +0100

    Testing

--- lib/timeouts_web/controllers/page_controller.ex
+++ lib/timeouts_web/controllers/page_controller.ex
@@ -2,9 +2,14 @@ defmodule TimeoutsWeb.PageController do
   use TimeoutsWeb, :controller
 
   def index(conn, _params) do
-    IO.puts("calling ping")
-    res = GenServer.call(Timeouts.MyGenServer, :ping)
-    IO.puts("ping call finished: #{inspect(res)}")
+    IO.puts("waiting...")
+    :timer.sleep(5_000)
+    IO.puts("Killing self...")
+    # This works...
+    Process.exit(self(), :normal)
+    # This doesn't
+    # Process.exit(self(), :kill)
+
     render(conn, "index.html")
   end
 end
