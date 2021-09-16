7de5d10c841ea9f950611345cab7586b7d1db9c3
Steven Anderson <sanderson@sopost.com>
2021-09-16 11:56:14 +0100

    Chagne back to timing out the genserver

--- lib/timeouts_web/controllers/page_controller.ex
+++ lib/timeouts_web/controllers/page_controller.ex
@@ -2,13 +2,15 @@ defmodule TimeoutsWeb.PageController do
   use TimeoutsWeb, :controller
 
   def index(conn, _params) do
-    IO.puts("waiting...")
-    :timer.sleep(5_000)
-    IO.puts("Killing self...")
-    # This works...
-    Process.exit(self(), :normal)
-    # This doesn't
-    # Process.exit(self(), :kill)
+    # IO.puts("waiting...")
+    # :timer.sleep(5_000)
+    # IO.puts("Killing self...")
+    # # This works...
+    # Process.exit(self(), :normal)
+    # # This doesn't
+    # # Process.exit(self(), :kill)
+
+    GenServer.call(Timeouts.MyGenServer, :ping)
 
     render(conn, "index.html")
   end
